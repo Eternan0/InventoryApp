39a37af60c9d4a98d57cc8a2d7bf45642bef2d5a
Sébastien Charron <sebastien.charron9@gmail.com>
2019-08-24 14:57:14 +0200

    trying to fix the create METHOD

--- routes/inventory.js
+++ routes/inventory.js
@@ -10,14 +10,3 @@ router.get('/:id', (item_controller.item_details))
 router.post('/create', (item_controller.item_create))
 
 module.exports = router;
-var router  = express.Router({mergeParams: true});
-var item_controller = require ('../controllers/itemsController')
-var Item = require ('../models/item')
-/* GET users listing. */
-router.get('/', (item_controller.item_list))
-
-router.get('/:id', (item_controller.item_details))
-
-router.post('/create', (item_controller.item_create))
-
-module.exports = router;
